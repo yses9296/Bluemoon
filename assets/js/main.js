850bff3453c97a395d85cbc5303ca22495b15345
Eunseo Choi <yses9296@gmail.com>
2022-09-27 02:08:31 +0900

    2022-09-26

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var menu_wrap = document.querySelector('.init_header'),
+        menu_icon = document.querySelector('.menu_icon'),
+        side_menu = document.querySelector('.side_menu'),
+        header_height = menu_wrap.offsetHeight;
+
+    side_menu.style.top = header_height + 'px';
+
+    menu_icon.addEventListener('click', e => {
+        e.preventDefault();
+        side_menu.classList.toggle('active');
+    })
+
+
+    var intro_cont = document.querySelectorAll('.intro_cont');
+    var intro_img = document.querySelectorAll('.intro_img');
+
+    window.addEventListener('scroll', function(){
+        var scrollTop =  this.scrollY ;
+
+        intro_cont.forEach((el, idx) => {
+            console.log('window offset: '+ scrollTop);
+            console.log('intro_cont offset: '+idx+ "-"+ el.offsetTop);
+
+            if(scrollTop >= el.offsetTop+500){
+                intro_img[idx].classList.add('active');
+            }
+            else {
+                intro_img[idx].classList.remove('active');
+            }
+        })
+    });
+
+});
